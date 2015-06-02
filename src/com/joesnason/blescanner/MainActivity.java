ee91f267fafe55963d05db2d1f49a09b5b5ec9c9
SungHangHsieh <joesnason@pool-70-109-2-99.atl.dsl-w.verizon.net>
2015-06-02 12:51:49 +0800

    Create Util.java

--- src/com/joesnason/blescanner/MainActivity.java
+++ src/com/joesnason/blescanner/MainActivity.java
@@ -169,7 +169,7 @@ public class MainActivity extends ActionBarActivity {
                 //Convert to hex String
                 byte[] uuidBytes = new byte[16];
                 System.arraycopy(scanRecord, startByte+4, uuidBytes, 0, 16);
-                String hexString = bytesToHex(uuidBytes);
+                String hexString = Util.bytesToHex(uuidBytes);
 
                 //Here is your UUID
 
@@ -195,19 +195,5 @@ public class MainActivity extends ActionBarActivity {
         }
     };
 
-    /**
-     * bytesToHex method
-     * Found on the internet
-     * http://stackoverflow.com/a/9855338
-     */
-    static final char[] hexArray = "0123456789ABCDEF".toCharArray();
-    private static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
+
 }
